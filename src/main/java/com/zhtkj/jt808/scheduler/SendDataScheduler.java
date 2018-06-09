0b400421a18af5f336f1fcbc0d93e2ce840c4363
HPBOOK <HPBOOK@HPBOOK-PC>
2018-06-09 09:55:29 +0800

    修复一些错误

--- src/main/java/com/zhtkj/jt808/scheduler/SendDataScheduler.java
+++ src/main/java/com/zhtkj/jt808/scheduler/SendDataScheduler.java
@@ -1,5 +1,7 @@
 package com.zhtkj.jt808.scheduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -9,23 +11,27 @@ import com.zhtkj.jt808.service.ServerMsgProcessService;
 @Component
 public class SendDataScheduler {
 
+	private static final Logger logger = LoggerFactory.getLogger(SendDataScheduler.class);
+	
 	@Autowired
 	private ServerMsgProcessService serverMsgProcessService;
 	
 	@Scheduled(cron = "10/15 * * * * ?")
-	public void sendActionData(){
+	public void sendActionData() {
 		try {
 			serverMsgProcessService.processSendActionMsg();
 		} catch (Exception e) {
+			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
 	}
 	
 	@Scheduled(cron = "12/15 * * * * ?")
-	public void sendParamData(){
+	public void sendParamData() {
 		try {
 			serverMsgProcessService.processSendParamMsg();
 		} catch (Exception e) {
+			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
 	}
--- src/main/java/com/zhtkj/jt808/service/ServerMsgProcessService.java
+++ src/main/java/com/zhtkj/jt808/service/ServerMsgProcessService.java
@@ -3,7 +3,6 @@ package com.zhtkj.jt808.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
 import com.zhtkj.jt808.common.JT808Const;
@@ -20,9 +19,6 @@ import io.netty.channel.Channel;
 @Service
 public class ServerMsgProcessService extends BaseMsgProcessService {
 
-    @Autowired
-    private TaskExecutor taskExecutor;
-    
 	@Autowired
     private MsgEncoder msgEncoder;
     
@@ -37,112 +33,101 @@ public class ServerMsgProcessService extends BaseMsgProcessService {
     
     //处理要发送给终端的指令
     public void processSendActionMsg() throws Exception {
-    	taskExecutor.execute(new Runnable() {
-            @Override
-            public void run() {
-            	List<DataAction> actions = dataActionMapper.findSendActionData();
-            	for (DataAction action: actions) {
-            		try {
-        				int actionType = action.getActionType();
-        				byte[] bodybs = null;
-        				if (actionType == 1) {
-        					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LOCKCAR, action);
-        				} else if (actionType == 2) {
-        					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LIMITSPEED, action);
-        				} else if (actionType == 3) {
-        					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LIMITUP, action);
-        				} else if (actionType == 6) {
-        					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_CONTROL, action);
-        				} else if (actionType == 7) {
-        					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LOCKCARCOMPANY, action);
-        				}
-        				//抓拍指令
-        				if (actionType == 4) {
-        					bodybs = msgEncoder.encode4CatchImgActionBody(JT808Const.ACTION_BODY_ID_CATCHIMG, action);
-        				}
-        				//密码指令
-        				if (actionType == 5) {
-        					List<CarRuntime> carRuntimes = carRuntimeMapper.findCarPassword(action.getPhoneNumber());
-        					if (carRuntimes.size() > 0 && carRuntimes.get(0).getCarPassport() != null) {
-        						bodybs = msgEncoder.encode4PasswordActionBody(
-        								JT808Const.ACTION_BODY_ID_PASSWORD, action, carRuntimes.get(0).getCarPassport());
-        					} else {
-        						continue;
-        					}
-        				}
-        				byte[] msgbs = msgEncoder.encode4Msg(JT808Const.ACTION_HEAD_ID, action.getPhoneNumber(), bodybs);
-        				Channel channel = sessionManager.getChannelByKey(action.getPhoneNumber());
-        				if (channel != null && channel.isActive()) {
-        					send2Terminal(channel, msgbs);
-        					dataActionMapper.updateActionReceiveResult(action.getActionId(), 1);
-        				} else {
-        					dataActionMapper.updateActionReceiveResult(action.getActionId(), -1);
-        				}
-        			} catch (Exception e) {
-        				dataActionMapper.updateActionReceiveResult(action.getActionId(), -1);
-        				e.printStackTrace();
-        			}
-            	}
-            }
-        });
+    	List<DataAction> actions = dataActionMapper.findSendActionData();
+    	for (DataAction action: actions) {
+    		try {
+    			//根据不同的指令类型打包消息体byte[]
+				int actionType = action.getActionType();
+				byte[] bodybs = null;
+				if (actionType == 1) {
+					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LOCKCAR, action);
+				} else if (actionType == 2) {
+					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LIMITSPEED, action);
+				} else if (actionType == 3) {
+					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LIMITUP, action);
+				} else if (actionType == 6) {
+					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_CONTROL, action);
+				} else if (actionType == 7) {
+					bodybs = msgEncoder.encode4SendActionBody(JT808Const.ACTION_BODY_ID_LOCKCARCOMPANY, action);
+				} else if (actionType == 4) { //抓拍指令
+					bodybs = msgEncoder.encode4CatchImgActionBody(JT808Const.ACTION_BODY_ID_CATCHIMG, action);
+				} else if (actionType == 5) { //密码指令
+					List<CarRuntime> carRuntimes = carRuntimeMapper.findCarPassword(action.getPhoneNumber());
+					if (carRuntimes.size() > 0 && carRuntimes.get(0).getCarPassport() != null) {
+						bodybs = msgEncoder.encode4PasswordActionBody(
+								JT808Const.ACTION_BODY_ID_PASSWORD, action, carRuntimes.get(0).getCarPassport());
+					} else {
+						continue;
+					}
+				}
+				//打包消息byte[]
+				byte[] msgbs = msgEncoder.encode4Msg(JT808Const.ACTION_HEAD_ID, action.getPhoneNumber(), bodybs);
+				Channel channel = sessionManager.getChannelByKey(action.getPhoneNumber());
+				if (channel != null && channel.isActive()) {
+					//发送byte[]给终端并更新receive_result状态
+					send2Terminal(channel, msgbs);
+					dataActionMapper.updateActionReceiveResult(action.getActionId(), 1);
+				} else {
+					dataActionMapper.updateActionReceiveResult(action.getActionId(), -1);
+				}
+			} catch (Exception e) {
+				dataActionMapper.updateActionReceiveResult(action.getActionId(), -1);
+				e.printStackTrace();
+			}
+    	}
     }
     
     //处理要发送给终端的参数
     public void processSendParamMsg() throws Exception {
-        taskExecutor.execute(new Runnable() {
-            @Override
-            public void run() {
-            	List<DataParam> params = dataParamMapper.findSendParamData();
-            	for (DataParam param: params) {
-            		try {
-        				int paramType = param.getParamType();
-        				byte[] bodybs = null;
-        				if (paramType == 1) {
-        					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_LINE, param);
-        				} else if (paramType == 2) {
-        					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_GONG, param);
-        				} else if (paramType == 3) {
-        					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_XIAO, param);
-        				} else if (paramType == 4) {
-        					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_LIMSPCIRCLE, param);
-        				} else if (paramType == 5) {
-        					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_PARKING, param);
-        				} else if (paramType == 6) {
-        					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_BAN, param);
-        				}
-        				if (paramType == 7) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_WORKPASSPORT, param);
-        				} else if (paramType == 8) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_INFO, param);
-        				} else if (paramType == 9) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_FINGER, param);
-        				} else if (paramType == 10) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_REDLIGHT, param);
-        				} else if (paramType == 11) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_DEVICECONFIG, param);
-        				} else if (paramType == 12) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_LOCKCAREXT, param);
-        				} else if (paramType == 13) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_CONTROLSWITCH, param);
-        				} else if (paramType == 14) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_THRESHOLDVALUE, param);
-        				} else if (paramType == 15) {
-        					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_NOTIFY, param);
-        				}
-        				byte[] msgbs = msgEncoder.encode4Msg(JT808Const.PARAM_HEAD_ID, param.getPhoneNumber(), bodybs);
-        				Channel channel = sessionManager.getChannelByKey(param.getPhoneNumber());
-        				if (channel != null && channel.isActive()) {
-        					send2Terminal(channel, msgbs);
-        					dataParamMapper.updateParamReceiveResult(param.getParamId(), 1);
-        				} else {
-        					dataParamMapper.updateParamReceiveResult(param.getParamId(), -1);
-        				}
-        			} catch (Exception e) {
-        				dataParamMapper.updateParamReceiveResult(param.getParamId(), -1);
-        				e.printStackTrace();
-        			}
-            	}
-            }
-        });
+    	List<DataParam> params = dataParamMapper.findSendParamData();
+    	for (DataParam param: params) {
+    		try {
+				int paramType = param.getParamType();
+				byte[] bodybs = null;
+				if (paramType == 1) {
+					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_LINE, param);
+				} else if (paramType == 2) {
+					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_GONG, param);
+				} else if (paramType == 3) {
+					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_XIAO, param);
+				} else if (paramType == 4) {
+					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_LIMSPCIRCLE, param);
+				} else if (paramType == 5) {
+					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_PARKING, param);
+				} else if (paramType == 6) {
+					bodybs = msgEncoder.encode4SendFenceParamBody(JT808Const.PARAM_BODY_ID_BAN, param);
+				}
+				if (paramType == 7) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_WORKPASSPORT, param);
+				} else if (paramType == 8) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_INFO, param);
+				} else if (paramType == 9) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_FINGER, param);
+				} else if (paramType == 10) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_REDLIGHT, param);
+				} else if (paramType == 11) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_DEVICECONFIG, param);
+				} else if (paramType == 12) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_LOCKCAREXT, param);
+				} else if (paramType == 13) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_CONTROLSWITCH, param);
+				} else if (paramType == 14) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_THRESHOLDVALUE, param);
+				} else if (paramType == 15) {
+					bodybs = msgEncoder.encode4SendDirectParamBody(JT808Const.PARAM_BODY_ID_NOTIFY, param);
+				}
+				byte[] msgbs = msgEncoder.encode4Msg(JT808Const.PARAM_HEAD_ID, param.getPhoneNumber(), bodybs);
+				Channel channel = sessionManager.getChannelByKey(param.getPhoneNumber());
+				if (channel != null && channel.isActive()) {
+					send2Terminal(channel, msgbs);
+					dataParamMapper.updateParamReceiveResult(param.getParamId(), 1);
+				} else {
+					dataParamMapper.updateParamReceiveResult(param.getParamId(), -1);
+				}
+			} catch (Exception e) {
+				dataParamMapper.updateParamReceiveResult(param.getParamId(), -1);
+				e.printStackTrace();
+			}
+    	}
     }
 }
