4ce49bb95aac75831365efd06f0438f7c264f449
HPBOOK <HPBOOK@HPBOOK-PC>
2018-06-14 11:32:52 +0800

    优化代码

--- src/main/java/com/zhtkj/jt808/entity/CarRuntime.java
+++ src/main/java/com/zhtkj/jt808/entity/CarRuntime.java
@@ -5,256 +5,194 @@ import java.util.Date;
 
 public class CarRuntime {
 
-	private String devPhone;
-
-	private byte boxClose;
-
-	private byte boxEmpty;
-
-	private byte boxUp;
-
 	private String carNumber;
-	
+	private String devPhone;
+	private byte boxClose;
+	private byte boxEmpty;
+	private byte boxUp;
 	private String driverId;
-
 	private String carPassport;
-
 	private String carPassword;
-
 	private String carState;
-
 	private byte carWeigui;
-
 	private float gpsDirect;
-
 	private float gpsHeight;
-
 	private BigDecimal gpsPosX;
-
 	private BigDecimal gpsPosY;
-
 	private float gpsSpeed;
-
 	private byte gpsValid;
-
 	private Date offlineDatetime;
-
 	private Date onlineDatetime;
-
-	private byte onlineState;
-
 	private Date reviceDatetime;
-
 	private Date sendDatetime;
-
-	
-	public String getDevPhone() {
-		return devPhone;
-	}
-
-	
-	public void setDevPhone(String devPhone) {
-		this.devPhone = devPhone;
-	}
-
-	
-	public byte getBoxClose() {
-		return boxClose;
-	}
-
-	
-	public void setBoxClose(byte boxClose) {
-		this.boxClose = boxClose;
-	}
-
-	
-	public byte getBoxEmpty() {
-		return boxEmpty;
-	}
-
-	
-	public void setBoxEmpty(byte boxEmpty) {
-		this.boxEmpty = boxEmpty;
-	}
-
-	
-	public byte getBoxUp() {
-		return boxUp;
-	}
-
-	
-	public void setBoxUp(byte boxUp) {
-		this.boxUp = boxUp;
-	}
-
+	private byte onlineState;
 	
 	public String getCarNumber() {
 		return carNumber;
 	}
-
 	
 	public void setCarNumber(String carNumber) {
 		this.carNumber = carNumber;
 	}
-
+	
+	public String getDevPhone() {
+		return devPhone;
+	}
+	
+	public void setDevPhone(String devPhone) {
+		this.devPhone = devPhone;
+	}
+	
+	public byte getBoxClose() {
+		return boxClose;
+	}
+	
+	public void setBoxClose(byte boxClose) {
+		this.boxClose = boxClose;
+	}
+	
+	public byte getBoxEmpty() {
+		return boxEmpty;
+	}
+	
+	public void setBoxEmpty(byte boxEmpty) {
+		this.boxEmpty = boxEmpty;
+	}
+	
+	public byte getBoxUp() {
+		return boxUp;
+	}
+	
+	public void setBoxUp(byte boxUp) {
+		this.boxUp = boxUp;
+	}
 	
 	public String getDriverId() {
 		return driverId;
 	}
-
 	
 	public void setDriverId(String driverId) {
 		this.driverId = driverId;
 	}
-
 	
 	public String getCarPassport() {
 		return carPassport;
 	}
-
 	
 	public void setCarPassport(String carPassport) {
 		this.carPassport = carPassport;
 	}
-
 	
 	public String getCarPassword() {
 		return carPassword;
 	}
-
 	
 	public void setCarPassword(String carPassword) {
 		this.carPassword = carPassword;
 	}
-
 	
 	public String getCarState() {
 		return carState;
 	}
-
 	
 	public void setCarState(String carState) {
 		this.carState = carState;
 	}
-
 	
 	public byte getCarWeigui() {
 		return carWeigui;
 	}
-
 	
 	public void setCarWeigui(byte carWeigui) {
 		this.carWeigui = carWeigui;
 	}
-
 	
 	public float getGpsDirect() {
 		return gpsDirect;
 	}
-
 	
 	public void setGpsDirect(float gpsDirect) {
 		this.gpsDirect = gpsDirect;
 	}
-
 	
 	public float getGpsHeight() {
 		return gpsHeight;
 	}
-
 	
 	public void setGpsHeight(float gpsHeight) {
 		this.gpsHeight = gpsHeight;
 	}
-
 	
 	public BigDecimal getGpsPosX() {
 		return gpsPosX;
 	}
-
 	
 	public void setGpsPosX(BigDecimal gpsPosX) {
 		this.gpsPosX = gpsPosX;
 	}
-
 	
 	public BigDecimal getGpsPosY() {
 		return gpsPosY;
 	}
-
 	
 	public void setGpsPosY(BigDecimal gpsPosY) {
 		this.gpsPosY = gpsPosY;
 	}
-
 	
 	public float getGpsSpeed() {
 		return gpsSpeed;
 	}
-
 	
 	public void setGpsSpeed(float gpsSpeed) {
 		this.gpsSpeed = gpsSpeed;
 	}
-
 	
 	public byte getGpsValid() {
 		return gpsValid;
 	}
-
 	
 	public void setGpsValid(byte gpsValid) {
 		this.gpsValid = gpsValid;
 	}
-
 	
 	public Date getOfflineDatetime() {
 		return offlineDatetime;
 	}
-
 	
 	public void setOfflineDatetime(Date offlineDatetime) {
 		this.offlineDatetime = offlineDatetime;
 	}
-
 	
 	public Date getOnlineDatetime() {
 		return onlineDatetime;
 	}
-
 	
 	public void setOnlineDatetime(Date onlineDatetime) {
 		this.onlineDatetime = onlineDatetime;
 	}
-
-	
-	public byte getOnlineState() {
-		return onlineState;
-	}
-
-	
-	public void setOnlineState(byte onlineState) {
-		this.onlineState = onlineState;
-	}
-
 	
 	public Date getReviceDatetime() {
 		return reviceDatetime;
 	}
-
 	
 	public void setReviceDatetime(Date reviceDatetime) {
 		this.reviceDatetime = reviceDatetime;
 	}
-
 	
 	public Date getSendDatetime() {
 		return sendDatetime;
 	}
-
 	
 	public void setSendDatetime(Date sendDatetime) {
 		this.sendDatetime = sendDatetime;
 	}
-		
+	
+	public byte getOnlineState() {
+		return onlineState;
+	}
+	
+	public void setOnlineState(byte onlineState) {
+		this.onlineState = onlineState;
+	}
+	
 }
